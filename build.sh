5ffd417d1a321b4bbb1aa72b3fc0b67b8ff9b0d8
Chathura Hapukotuwa <hapukot-se22049@stu.kln.ac.lk>
2025-06-24 14:33:21 +0530

    style:Added messages to displayed in the terminal

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+
+echo "--- Assembling Bootloader ---"
 nasm -f bin bootload.asm -o boot.bin
+
+echo "--- Assembling Kernel ---"
 nasm -f bin kernel.asm -o kernel.bin
+
+echo "--- Combining to create disk image ---"
 cat boot.bin kernel.bin > chathuraos.img
+
+echo "--- Booting with QEMU ---"
 qemu-system-x86_64 -fda chathuraos.img
